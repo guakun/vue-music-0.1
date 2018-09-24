5efd0cab2c8ea2ae35a9b487e57272b09934d87c
guakun <2306831967@qq.com>
2018-09-24 14:05:33 +0800

    更新 slider 的 dom

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot></slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MySlider'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
