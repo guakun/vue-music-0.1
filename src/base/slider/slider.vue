939bc86b4d4582b92a03fb2917618f1a2950988c
guakun <2306831967@qq.com>
2018-09-24 14:46:36 +0800

    完成 slider 的初始化

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
     <div class="dots"></div>
@@ -8,6 +8,9 @@
 </template>
 
 <script>
+import { addClass } from 'common/js/dom'
+import BScroll from 'better-scroll'
+
 export default {
   name: 'MySlider',
   props: {
@@ -22,6 +25,45 @@ export default {
     interval: {
       type: Number,
       default: 3000
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this._setSliderWidth()
+      this._initSlider()
+    }, 20)
+  },
+  methods: {
+    _setSliderWidth () {
+      this.children = this.$refs.sliderGroup.children
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      console.log('children')
+      console.log(this.children)
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+
+      if (this.loop) {
+        width += sliderWidth * 2
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initSlider () {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: true,
+        snapLoop: this.loop,
+        snapThreshold: 0.3,
+        snapSpeed: 400,
+        click: true
+      })
     }
   }
 }
