57228e624210c13c1ee3d450d5fc52b1b734dcbf
guakun <2306831967@qq.com>
2018-09-24 11:33:47 +0800

    添加 tab 标签

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <my-header></my-header>
+    <my-tab></my-tab>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MyHeader from 'components/m-header/m-header'
+import MyTab from 'components/tab/tab'
 export default {
   name: 'App',
   components: {
-    MyHeader
+    MyHeader,
+    MyTab
   }
 }
 </script>
