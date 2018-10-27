261834d2ba932784372345e2ab4044d078eac3a5
wanghongkun <wanghongkun@corp.netease.com>
2018-10-27 18:19:25 +0800

    写到添加滑动事件啦

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,7 +1,7 @@
 <template>
-  <scroll class="listview" :data="data">
+  <scroll class="listview" :data="data" ref="listview">
     <ul>
-      <li v-for="(group, index) in data" class="list-group" :key="index">
+      <li v-for="(group, index) in data" class="list-group" :key="index" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="(item, index) in group.items" class="list-group-item" :key="index">
@@ -11,22 +11,45 @@
         </ul>
       </li>
     </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" class="item" :data-index="index" :key="item">{{item}}</li>
+      </ul>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import { getData } from 'common/js/dom'
 
 export default {
   name: 'ListView',
+  components: {
+    Scroll
+  },
   props: {
     data: {
       type: Array,
       default: () => []
     }
   },
-  components: {
-    Scroll
+  computed: {
+    shortcutList () {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
+    }
+  },
+  methods: {
+    onShortcutTouchMove (e) {
+      // let first
+    },
+    onShortcutTouchStart (e) {
+      let anchorIndex = getData(e.target, 'index')
+      // let firstTouch = e.touches[0]
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[anchorIndex], 0)
+    }
   }
 }
 </script>
@@ -61,4 +84,23 @@ export default {
         margin-left 20px
         color $color-text-l
         font-size $font-size-medium
+  .list-shortcut
+    // border 1px solid red
+    position absolute
+    right 0
+    top 50%
+    transform translateY(-50%)
+    width 20px
+    padding 20px 0
+    border-radius 10px
+    background $color-background-d
+    text-align center
+    font-family Helvetica
+    .item
+      padding 3px
+      line-height 1
+      color $color-text-l
+      font-size $font-size-small
+      &.current
+        color $color-theme
 </style>
