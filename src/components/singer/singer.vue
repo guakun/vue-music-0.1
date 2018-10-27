ca8e7542e7a05258184534e6dfb84913b63c5a48
wanghongkun <wanghongkun@corp.netease.com>
2018-10-27 13:23:31 +0800

    数据处理

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,87 @@
 <template>
   <div class="singer">
-    singer
   </div>
 </template>
 
 <script>
-export default {
+import { getSingerList } from 'api/singer'
+import { ERROR_OK } from 'api/config'
+import Singer from 'common/js/singer'
 
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  name: 'SingerList',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERROR_OK) {
+          this.singers = res.data.list
+          this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: 
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 为了得到有序列表, 需要来处理 map
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        // 升序
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+.singer
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
+  border: 1px solid red
 </style>
